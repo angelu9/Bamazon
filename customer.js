c27286692b3e81eb538639b0c53f63d5dbc2dc19
angel m <angelmartinez@Angels-MacBook-Pro.local>
2019-11-02 10:31:37 -0400

    update

--- customer.js
+++ customer.js
@@ -8,8 +8,8 @@ var cTable = require('console.table');
 var connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: 'acmilan10',
+    user: '',
+    password: '',
     database: 'bamazonDB'
 });
 
